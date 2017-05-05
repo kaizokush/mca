3cfde605766d150899d5697d1301143ca80befac
Ujjwal <ushakya3@gmail.com>
2017-05-05 15:50:13 +0545

    logic added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.create(post_params)
     if @post.valid?
-      redirect_to thank_path
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
